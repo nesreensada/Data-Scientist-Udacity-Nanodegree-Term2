62d6244e8db938b73e6b07535ba1bc07a1a5890a
nesreensada <nesreensada@gmail.com>
2019-08-05 07:25:20 +0300

    update: completed the script for the train classifier including loading data from db, building a model including optimizing the parameters and saving the trained model

--- Disaster_Pipeline /models/train_classifier.py
+++ Disaster_Pipeline /models/train_classifier.py
@@ -1,24 +1,126 @@
+# import libraries
 import sys
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+from string import punctuation
+import numpy as np
 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.ensemble import AdaBoostClassifier
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.externals import joblib
+
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
-
+    """
+    This function loads data from given database path 
+    and returns a dataframe
+    Input:
+        database_filepath: database file path
+    Output:
+        X: traing message list
+        Y: training target
+        category names  
+    """
+    # load data from database
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table('messages',engine)
+    
+    # define features and target
+    X = df.message
+    y = df.iloc[:,4:]
+    category_names = list(df.columns[4:])
+    
+    return X, y, category_names
 
 def tokenize(text):
-    pass
+    """
+    Tokenization function to process the text data to normalize, lemmatize, and tokenize text. 
+    Input: Text data
+    Output: List of clean tokens 
+    """
+     # remove punctations
+    text =  ''.join([c for c in text if c not in punctuation])
+    
+    #tokenize text
+    tokens = word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for token in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 def build_model():
-    pass
-
+    """
+    Build Machine learning pipleine using adaboost classifier
+    Input:
+       None
+    Output: 
+        clf: gridSearch Model
+    """
+    ada_pipeline =  Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier((AdaBoostClassifier())))
+    ])
+    # grid search parameters
+    parameters = {
+    'tfidf__norm':['l2','l1'],
+    'vect__stop_words': ['english',None],
+    'clf__estimator__learning_rate' :[0.1, 0.5, 1, 2],
+    'clf__estimator__n_estimators' : [50, 60, 70],
+    }
+    #create grid search object
+    clf_grid_model = GridSearchCV(ada_pipeline, parameters)
+    return clf_grid_model
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Prints the classification report for the given model and test data
+    Input:
+        model: trained model
+        X_test: test data for the predication 
+        Y_test: true test labels for the X_test data
+    Output:
+        None 
+    """
+    # predict 
+    y_pred = model.predict(X_test)
+    # print the metrics
+    for i, col in enumerate(category_names):
+        print('{} category metrics: '.format(col))
+        print(classification_report(Y_test.iloc[:,i], y_pred[:,i]))
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    This method is used to export a model as a pickle file
+    Input:
+        model: trained model 
+        model_filepath: location to store the model
+    Output: None
+    """
+    joblib.dump(model, model_filepath)
 
 
 def main():
